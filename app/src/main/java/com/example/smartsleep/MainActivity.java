661da7da30fc62f5c8269bf417479f8637a15b28
Alexander Spinelli <aspinelli01@manhattan.edu>
2020-12-01 02:07:23 -0500

    Fixed Colors

--- app/src/main/java/com/example/smartsleep/MainActivity.java
+++ app/src/main/java/com/example/smartsleep/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // Find View for HeartRate
         TextView heartRate = (TextView) findViewById(R.id.heart_rate);
         //Set HeartRate click
