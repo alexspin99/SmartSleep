3ac31b424fdc136b500b20092e5d2fcd5bb353bc
Alexander Spinelli <aspinelli01@manhattan.edu>
2021-01-27 16:25:06 -0500

    -Adds BLE service (needs to be edited to include all services and characteristics used by device) -Cleans up manifest code

---
+++ app/src/main/java/com/example/smartsleep/GattAttributes.java
@@ -0,0 +1,33 @@
+package com.example.smartsleep;
+
+import java.util.HashMap;
+
+/**
+ * This class includes a HashMap of GATT attributes being sent to the application.
+ */
+
+
+//TODO: customize attributes to include everything being sent to device
+
+public class GattAttributes {
+
+    private static HashMap<String, String> attributes = new HashMap();
+
+    //uuid variables
+    public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+
+    static {
+        // Services
+        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
+        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        // Characteristics
+        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+    }
+
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
+}
